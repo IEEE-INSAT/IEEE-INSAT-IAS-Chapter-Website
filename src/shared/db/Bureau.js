f76c466e3093194eec3fcad5142ce92d0d89c83e
tarek-gritli <tarek.gritli@insat.ucar.tn>
2025-10-02 19:28:53 +0100

    update board pictures

--- src/shared/db/Bureau.js
+++ src/shared/db/Bureau.js
@@ -5,7 +5,7 @@ const members = [
       position: "Chair",
       facebook: "https://www.facebook.com/aziz.derbali.1",
       image:
-        "https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-6/493890072_9904135999680174_874542530890162936_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=VMbeCn5A3c8Q7kNvwGUGTHX&_nc_oc=AdkthPfdmf6qZ0lbE68oRugrqiplyISDXxWkREFyJLczg3sboLCYTG7cePT-5iYEr2w&_nc_zt=23&_nc_ht=scontent.ftun9-1.fna&_nc_gid=oh1FImnWI9HKUlZqg9M3HQ&oh=00_AfYsZaGXJ3t7TsN4behuEoc64Adi0SShb2haiUqGQNzKGA&oe=68E46A88",
+        "https://res.cloudinary.com/dndykifci/image/upload/v1759428991/WhatsApp_Image_2025-09-24_%C3%A0_17.04.59_9a8561f2_nkuzfa.jpg",
     },
   ],
   [
@@ -14,14 +14,14 @@ const members = [
       position: "Vice chair membership activities",
       facebook: "https://www.facebook.com/moulahi.skn",
       image:
-        "https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-6/490611399_1464912908218609_7150471049097934348_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=i7wSWp9PZaoQ7kNvwH8-f6q&_nc_oc=AdltP-ap_OqrVkSjWmKbf6KziNC7DiQXuUHLMYt4AMK1NH3FF49ggwAryzTgEFRB5OI&_nc_zt=23&_nc_ht=scontent.ftun9-1.fna&_nc_gid=FRUTOeKbQq50uuPft8vFOw&oh=00_AfaaWEK1e84U_UA4AzD2U_3zL6u_ESxL64JrJKTCVkji_w&oe=68E48579",
+        "https://res.cloudinary.com/dndykifci/image/upload/v1759428991/WhatsApp_Image_2025-09-24_%C3%A0_17.04.59_2a44686a_awcq2m.jpg",
     },
     {
       name: "Badie Sakka",
       position: "Vice chair technical activities",
       facebook: "https://www.facebook.com/BadieSakka",
       image:
-        "https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-1/489328274_9883980831652578_5383697925501848546_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=107&ccb=1-7&_nc_sid=1d2534&_nc_ohc=KEbqgQxWd3AQ7kNvwGAb3Bg&_nc_oc=AdlFxXsb3G13ZMx4Gsc7lS7fzZXc67bMJ9HJIUDZ_KAtxBwfblvdNb9oKLjMsVLrGPQ&_nc_zt=24&_nc_ht=scontent.ftun9-1.fna&_nc_gid=yrhhclTnraoMrOXM7sy0AQ&oh=00_AfYLd3BYbC0qU95d2BPCI7shRt1A9uL-a24KiI0JXru_4w&oe=68E46B10",
+        "https://res.cloudinary.com/dndykifci/image/upload/v1759428990/WhatsApp_Image_2025-09-24_%C3%A0_17.04.58_00272895_xf3rfe.jpg",
     },
   ],
   [
@@ -30,14 +30,14 @@ const members = [
       position: "General secretary",
       facebook: "https://www.facebook.com/maram.bensaid.5",
       image:
-        "https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-1/505363215_3627417777390976_1612429420280384610_n.jpg?stp=dst-jpg_s100x100_tt6&_nc_cat=104&ccb=1-7&_nc_sid=e99d92&_nc_ohc=zYaEbATNIQ8Q7kNvwG6R-rN&_nc_oc=AdlWH0w0DG-Z-KkPiy-rVhE7uIwOOijQZUMkyTxz2quI8Yu9sfHenK3T0m79Z9aS_Co&_nc_zt=24&_nc_ht=scontent.ftun9-1.fna&_nc_gid=nbPHvRxzeBD9Y_VvCjQRiw&oh=00_AfZVK6w40Qb-8Y_AAiwcQht2Ecrg8t9-LMRSm2XbJCDmKg&oe=68E45E2F",
+        "https://res.cloudinary.com/dndykifci/image/upload/v1759428991/WhatsApp_Image_2025-09-24_%C3%A0_23.04.07_4cee26ce_dyyl8e.jpg",
     },
     {
       name: "Hiba Elghali",
       position: "Treasurer",
       facebook: "https://www.facebook.com/hibaleghali23",
       image:
-        "https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-1/480105974_4010866395815437_4900923485988304589_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=111&ccb=1-7&_nc_sid=e99d92&_nc_ohc=oJnP-ROg5oQQ7kNvwHvuLm0&_nc_oc=Adm4ODtnUKeoWG5-A3XdwRh8jGiMzGk-lMQ6Z860o2AWxCgNSdrbs3Ot5sUQdJyAheI&_nc_zt=24&_nc_ht=scontent.ftun9-1.fna&_nc_gid=US1Yz_kZK40-bdLXbkquiQ&oh=00_AfZsUMdvTQl5IHoQxVnKb1rJJz8SfwS5qStAPe60bJ0KBg&oe=68E48911",
+        "https://res.cloudinary.com/dndykifci/image/upload/v1759428861/a_qcnsx0.webp",
     },
   ],
   [
@@ -46,14 +46,14 @@ const members = [
       position: "Media Manager",
       facebook: "https://www.facebook.com/search/top?q=aissaoui%20med%20amine",
       image:
-        "https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-1/508310463_122165566388456796_8145922554549517679_n.jpg?stp=dst-jpg_s100x100_tt6&_nc_cat=101&ccb=1-7&_nc_sid=e99d92&_nc_ohc=GW9LnFjRsYoQ7kNvwES-i8Q&_nc_oc=AdmtJmMZbrdpGxibzc2MID5Y7HvIrdSVJY9FSw6ebS3cZDi2-gU0H9ElVyWMghNNFFw&_nc_zt=24&_nc_ht=scontent.ftun9-1.fna&_nc_gid=y8Em3skXVfkrlEANQb_1Ig&oh=00_AfZfLHt3KRCEwkVcqPfcfQj5mmF_KUGOfTQJsf7bp7Qtdg&oe=68E46C2B",
+        "https://res.cloudinary.com/dndykifci/image/upload/v1759428991/WhatsApp_Image_2025-09-24_%C3%A0_17.04.58_d4a48af1_aswlei.jpg",
     },
     {
       name: "Haroun Ben Romdhane",
       position: "Logistics & Industrial Visits Manager",
       facebook: "https://www.facebook.com/haroun.benromdhan.2025/",
       image:
-        "https://scontent.ftun9-1.fna.fbcdn.net/v/t1.6435-1/80862928_884832418585240_4957582609770086400_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=101&ccb=1-7&_nc_sid=e99d92&_nc_ohc=9yuEQoAIpIEQ7kNvwGeEeDm&_nc_oc=Adlhxa30EwyP4cyeB047objFmnhDyTFjfIJ5JcIGjhOK70kTpWPnTS51GRjCmtdesKk&_nc_zt=24&_nc_ht=scontent.ftun9-1.fna&_nc_gid=N5tYrk6-63-zArnhCDQ4cA&oh=00_AfYzvXYdZSFt7rkL6adXfRfvJl6KpeOj97mFPoI1zCGtIg&oe=69061E18",
+        "https://res.cloudinary.com/dndykifci/image/upload/v1759428990/WhatsApp_Image_2025-09-24_%C3%A0_17.04.57_98e6e600_uyhz7w.jpg",
     },
   ],
 ];
